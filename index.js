6689f6b36dd3667257f2e550979bbb8a7097f194
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-03-21 22:57:44 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+
+    return customerName = customerName.toUpperCase()
+    
+}
+
+function setBestCustomer(){
+
+    return bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    
+    return bestCustomer = "maybe bob"
+    
+}
+
+const leastFavoriteCustomer = "not Tamnai"
+
+function changeLeastFavoriteCustomer() {
+
+    return leastFavoriteCustomer = "bob"
+    
+}
